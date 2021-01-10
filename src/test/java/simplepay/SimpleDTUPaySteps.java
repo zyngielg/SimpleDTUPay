42f1af4e85bdebecc8d053226bde65de5fa9fd82
zyngielg <gustaw.zyngiel@gmail.com>
2021-01-10 21:40:17 +0100

    progress with cucumber

--- src/test/java/simplepay/SimpleDTUPaySteps.java
+++ src/test/java/simplepay/SimpleDTUPaySteps.java
@@ -1,5 +1,16 @@
 package simplepay;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.platform.engine.Cucumber;
+import simplepay.exceptions.CustomerInsufficientFundsException;
+import simplepay.exceptions.CustomerNotFoundException;
+import simplepay.exceptions.MerchantNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Cucumber
 public class SimpleDTUPaySteps {
     String cid, mid;
     SimpleDTUPay dtuPay = new SimpleDTUPay();
@@ -14,7 +25,11 @@ public class SimpleDTUPaySteps {
     }
     @When("the merchant initiates a payment for {int} kr by the customer")
     public void theMerchantInitiatesAPaymentForKrByTheCustomer(int amount) {
-        successful = dtuPay.pay(amount,cid,mid);
+        try {
+            successful = dtuPay.pay(amount,cid,mid);
+        } catch (MerchantNotFoundException | CustomerNotFoundException | CustomerInsufficientFundsException e) {
+            e.printStackTrace();
+        }
     }
     @Then("the payment is successful")
     public void thePaymentIsSuccessful() {
